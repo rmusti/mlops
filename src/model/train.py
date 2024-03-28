05aa7cd56d764a3bf6268be34c33ab099ae32961
gitName <a@b.c>
2024-03-28 14:03:19 -0400

    Small Change

--- src/model/train.py
+++ src/model/train.py
@@ -13,7 +13,7 @@ from sklearn.model_selection import train_test_split
 
 # define functions
 def main(args):
-    # TO DO: enable autologging
+    # TO DO: enable autologging sf
     mlflow.autolog()
 
 
