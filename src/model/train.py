9179f7a17f96ed4d37446b75c47a0cc157cb5075
gitName <a@b.c>
2024-03-30 20:12:50 -0400

    Refactor split_data function to improve code readability and maintainability

--- src/model/train.py
+++ src/model/train.py
@@ -37,13 +37,13 @@ def get_csvs_df(path):
 
 # TO DO: add function to split data
 def split_data(df):
-    X, y = df[['Pregnancies', 
-               'PlasmaGlucose', 
-               'DiastolicBloodPressure', 
-               'TricepsThickness', 
-               'SerumInsulin', 
-               'BMI', 
-               'DiabetesPedigree', 
+    X, y = df[['Pregnancies',
+               'PlasmaGlucose',
+               'DiastolicBloodPressure',
+               'TricepsThickness',
+               'SerumInsulin',
+               'BMI',
+               'DiabetesPedigree',
                'Age']].values, df['Diabetic'].values
 
     X_train, X_test, y_train, y_test = train_test_split(
@@ -71,6 +71,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
